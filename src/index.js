7102eaa4be6cc46169ea21ef4d67c125e99daead
Yashar <yashar22mo@gmail.com>
2024-07-17 11:48:33 +0330

    git initialize

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+// FIRST WAY TO USE REACT
+// ReactDOM.render(
+//     <Main />,
+//     document.getElementById('root')
+// );
+
+// SECOND WAY TO USE REACT
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.Fragment>
+
+	</React.Fragment>
+);
